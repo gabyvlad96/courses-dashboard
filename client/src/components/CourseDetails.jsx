ca630b8a72c35fb665cc26d3161400c404f442e7
Gabriel Vlad <gabyvlad96@gmail.com>
2021-10-28 09:58:03 +0300

    add engagement/popularity graph for users and courses

--- client/src/components/CourseDetails.jsx
+++ client/src/components/CourseDetails.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { baseUrl, months, prepareChartData } from '../utils/utils';
 import './courseDetails.css';
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const chartOptions = {
+    scales: { y: { ticks: { stepSize: 1 }}}
+};
 
 const CourseDetails = ({ course }) => {
     const [courseData, setCourseData] = useState({
         avgCompletionTime: null,
         fastestUser: null,
         fastestTime: null,
+        yearCount: null,
       });
     useEffect(async () => {
         try {
-            const response = await fetch(`${BASE_URL}/course/${course.id}`);
+            const response = await fetch(`${baseUrl}/course/${course.id}`);
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-            const [avgCompletionTime, fastestUser, fastestTime] = await response.json();
+            const [avgCompletionTime, fastestUser, fastestTime, yearCount] = await response.json();
             setCourseData({
                 avgCompletionTime,
                 fastestUser,
                 fastestTime,
+                yearCount,
             })
             console.log(courseData);
 		} catch (error) {
@@ -26,9 +32,9 @@ const CourseDetails = ({ course }) => {
     }, [course]);
 
     return (
-        <div className="courseDetails-main">
+        <div className="course-details-main">
             {courseData.avgCompletionTime &&
-                <div className="completionTime card">
+                <div className="completion-time card">
                     <h3>Average completion time</h3>
                     <h1>{courseData.avgCompletionTime.split(',')[0]}</h1>
                     <h2>
@@ -38,13 +44,22 @@ const CourseDetails = ({ course }) => {
                 </div>
             }
             {courseData.fastestUser &&
-                <div className="fastestTime card">
+                <div className="fastest-time card">
                     <h3>Fastest completion by</h3>
                     <h1>{courseData.fastestUser.firstName} {courseData.fastestUser.lastName}</h1>
                     <h2>
                         {parseInt(courseData.fastestTime.split(':')[0]) || 0} hours and&nbsp;
                         {parseInt(courseData.fastestTime.split(':')[1]) || 0} minutes
                     </h2>
+                </div>
+            }
+            {courseData.yearCount && 
+                <div className="course-chart card">
+                    <Line
+                        data={prepareChartData(courseData.yearCount, months, "Course popularity")}
+                        responsive={true}
+                        options={chartOptions}
+                    />
                 </div>
             }
         </div>
--- client/src/components/UserDetails.jsx
+++ client/src/components/UserDetails.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { baseUrl, months, prepareChartData } from '../utils/utils';
 import './userDetails.css';
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const chartOptions = {
+    scales: { yAxes: { max: 5, ticks: { stepSize: 1 }}}
+};
 
 const UserDetails = ({ user }) => {
-    const [courses, setCourses] = useState('');
+    const [courses, setCourses] = useState(null);
+    const [yearCount, setYearCount] = useState(null);
 
     useEffect(async () => {
         try {
-            const response = await fetch(`${BASE_URL}/user/${user.id}`);
+            const response = await fetch(`${baseUrl}/user/${user.id}`);
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-            const courses = await response.json()
-			setCourses(courses);
+            const [ courses, yearCount ] = await response.json()
+            setCourses(courses);
+            setYearCount(yearCount)
             console.log(courses);
 		} catch (error) {
 			console.log(error);
@@ -34,9 +40,18 @@ const UserDetails = ({ user }) => {
                 <h3>Courses:</h3>
                 <ul>
                     {courses && courses.map((course, i) => (
-                        <li key={i}>{course.title}</li>
+                        <li key={i}><div>{i+1}</div> {course.title}</li>
                     ))}
                 </ul>
+            </div>
+            <div className="user-chart card">
+                {yearCount && 
+                    <Line
+                        data={prepareChartData(yearCount, months, "User engagement in 2021")}
+                        responsive={true}
+                        options={chartOptions}
+                    />
+                }
             </div>
         </div>
     )
